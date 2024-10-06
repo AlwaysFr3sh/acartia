91868e239af5f04eb3d67a1864c27f7d84c09b3d
dayniel19 <dancorwong19@gmail.com>
2024-10-06 16:45:09 +1100

    partner page update

--- src/client/src/components/Pages/PartnersPage.vue
+++ src/client/src/components/Pages/PartnersPage.vue
@@ -12,55 +12,48 @@
         </div>
         
         <div class="partners">
-            <mdb-container id="container">
-                <mdb-row>
-                    <div class="col-3 border mb-5 mr-5 rounded-xl" id="borders">
-                        <mdb-col col="sm"><img src="../../assets/orcasound.png" alt="Orcasound" id="orcasound"></mdb-col>
-                        <div class="icon1"><a href="https://www.orcasound.net/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
-                    </div>
-                    <div class="col-3 border mb-5 mr-5 rounded-xl">
-                        <mdb-col col="sm"><img src="../../assets/orcanetwork.png" alt="Orca Network" id="orcanetwork"></mdb-col>
-                        <div class="icon2"><a href="https://www.orcanetwork.org/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
-                    </div>
-                    <div class="col-3 border mb-5 mr-5 rounded-xl">
-                        <mdb-col col="sm"><img src="../../assets/typehuman.png" alt="TypeHuman" id="typehuman"></mdb-col>
-                        <div class="icon3"><a href="https://www.typehuman.com/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
-                    </div>
-                </mdb-row>
-                <mdb-row>
-                    <div class="col-3 border mb-5 mr-5 rounded-xl">
-                        <mdb-col col="sm"><img src="../../assets/conserveio.png" alt="Conserve.io" id="conserve"></mdb-col>
-                        <div class="icon4"><a href="https://conserve.io/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
-                    </div>
-                    <div class="col-3 border mb-5 mr-5 rounded-xl">
-                        <mdb-col col="sm"><img src="../../assets/resolve.png" alt="Resolve Conservation" id="rc"></mdb-col>
-                        <div class="icon5"><a href="https://www.resolveconservation.com/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
-                    </div>
-                    <div class="col-3 border mb-5 mr-5 rounded-xl">
-                        <mdb-col col="sm"><img src="../../assets/beamreach.png" alt="Beam Reach" id="br"></mdb-col>
-                        <div class="icon6"><a href="https://beamreach.blue/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
-                    </div>
-                </mdb-row>
-            </mdb-container>
-            <mdb-row>
-                <mdb-col col="12">
-                    <div class="page-links">
-                        <a href="https://github.com" class="link2">See our partners data contributions<img src="@/assets/right-arrow.svg" alt="Right Arrow" class="rightarrow"/></a>
-                    </div>
-                </mdb-col>
-            </mdb-row>
+            <div class="row">
+                <div class="box">
+                    <div class="img"><img src="../../assets/orcasound.png" alt="Orcasound" id="orcasound"></div>
+                    <div class="sm"><a href="https://www.orcasound.net/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
+                </div>
+                <div class="box">
+                    <div class="img"><img src="../../assets/orcanetwork.png" alt="Orca Network" id="orcanetwork"></div>
+                    <div class="sm"><a href="https://www.orcanetwork.org/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
+                </div>
+                <div class="box">
+                    <div class="img"><img src="../../assets/typehuman.png" alt="TypeHuman" id="typehuman"></div>
+                    <div class="sm"><a href="https://www.typehuman.com/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="box">
+                    <div class="img"><img src="../../assets/conserveio.png" alt="Conserve.io" id="conserve"></div>
+                    <div class="sm"><a href="https://conserve.io/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
+                </div>
+                <div class="box">
+                    <div class="img"><img src="../../assets/resolve.png" alt="Resolve Conservation" id="rc"></div>
+                    <div class="sm"><a href="https://www.resolveconservation.com/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
+                </div>
+                <div class="box">
+                    <div class="img"><img src="../../assets/beamreach.png" alt="Beam Reach" id="br"></div>
+                    <div class="sm"><a href="https://beamreach.blue/" class="sm"><img src="@/assets/plus-icon.svg" alt="Plus Icon" class="icon"/>See more</a></div>
+                </div>
+            </div>
+            <div>
+                <div class="page-links">
+                    <a href="https://github.com" class="link2">See our partners data contributions<img src="@/assets/right-arrow.svg" alt="Right Arrow" class="rightarrow"/></a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import {mdbContainer, mdbRow, mdbCol} from 'mdbvue';
+import {} from 'mdbvue';
 
   export default {
     components: {
-      mdbContainer,
-      mdbRow,
-      mdbCol
     }
   }
 </script>
@@ -79,12 +72,10 @@ import {mdbContainer, mdbRow, mdbCol} from 'mdbvue';
     padding-bottom: 108px;
 }
 
-#container{
-    width: auto;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    margin-left: 25%;
+.partners{
+    width: 1128px;
+    height: auto;
+    margin: auto;
 }
 
 .partner-desc h2 {
@@ -118,6 +109,33 @@ import {mdbContainer, mdbRow, mdbCol} from 'mdbvue';
     margin-top: 24px;
 }
 
+.row {
+    display: inline-flex;
+    gap: 54px;
+    margin-bottom: 54px;
+}
+
+.box {
+    width: 343px;
+    height: 409px;
+    font-weight: 500;
+    font-family: 'Montserrat';
+    font-size: 16px;
+    border-radius: 16px;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+    border-style: hidden !important;
+    display: inline-block;
+}
+
+.img {
+    width: 262.08px;
+    height: 343px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+}
+
 a:link {
     color: black;
 }
@@ -141,70 +159,29 @@ a:active {
     margin-bottom: 3px;
 }
 
-.icon1 {
-    margin-top: 20%;
-    margin-bottom: 10%;
-    text-align: center;
-}
-
-.icon2 {
-    margin-top: 37%;
-    text-align: center;
-}
-
-.icon3 {
-    margin-top: 50%;
-    text-align: center;
-}
-
-.icon4 {
-    margin-top: 50%;
-    margin-bottom: 10%;
-    text-align: center;
-}
-
-.icon5 {
-    margin-top: 30%;
-    font-weight: 500;
-    text-align: center;
-}
-
-.icon6 {
-    margin-top: 15%;
-    font-weight: 500;
-    text-align: center;
-}
-
 .sm {
-    font-weight: 500;
-    font-family: 'Montserrat';
-    font-size: 16px;
-
-}
-
-.rounded-xl {
-    border-radius: 16px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
-    border-style: hidden !important;
+    width: auto;
+    height: auto;
+    text-align: center;
+    padding: 10px 12px 10px 12px;
+    margin: auto;
 }
 
 #orcasound {
     width: 110%;
     height: auto;
-    padding-top: 10%;
-    padding-right: 10%;
+
 }
 
 #orcanetwork {
     width: 100%;
     height: auto;
-    padding-top: 30%;
+
 }
 
 #typehuman {
     width: 100%;
     height: auto;
-    padding-top: 50%;
 }
 
 #conserve {
@@ -228,6 +205,7 @@ a:active {
 .page-links {
     padding: 10px 12px 10px 12px;
     margin-bottom: 54px;
+    text-align: center;
 }
 
 .link2 {
@@ -236,7 +214,6 @@ a:active {
     font-size: 16px;
     line-height: 22.4px;
     display: inline-block;
-    margin-left: 42%;
 }
 
 .rightarrow {
